202686219f8f7802c8b82478e1dbcee57e5356e9
Nate Mara <natemara@fastmail.com>
2020-12-14 21:38:35 -0800

    Fix CSV python code, add readme to CSV Rust code

--- chapter-01/listing_01/main.py
+++ chapter-01/listing_01/main.py
@@ -1,6 +1,19 @@
 import time
 
 
+def sum_csv_column(data, column):
+  sum = 0
+
+  for line in data.split("\n"):
+    if len(line) == 0:
+      continue
+
+    value_str = line.split(",")[column]
+    sum += int(value_str)
+
+  return sum
+
+
 with open('data.csv') as f:
     data = f.read()
 
