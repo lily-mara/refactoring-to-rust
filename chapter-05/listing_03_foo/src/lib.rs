d7f71cc7b1a8a910cba5cf8b781b56a7dd0f3b4b
Lily Mara <lilymara@fastmail.com>
2021-05-25 19:59:09 -0700

    update types

--- chapter-05/listing_03_foo/src/lib.rs
+++ chapter-05/listing_03_foo/src/lib.rs
@@ -1,12 +1,12 @@
 use http::{Request, Response, StatusCode};
-use nginx::handlers;
+use nginx::{handlers, RequestBody};
 
 handlers! {
   "/calculate" => calculator,
 }
 
-fn calculator(request: Request<String>) -> Response<String> {
-  match calculate::evaluate(request.body()) {
+fn calculator<'a>(request: Request<RequestBody<'a>>) -> Response<String> {
+  match calculate::evaluate(request.body().as_str()) {
     Ok(result) => Response::builder()
       .status(StatusCode::OK)
       .body(format!("{}", result))
